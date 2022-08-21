992b6d5fef4a48c8a5eaca88ffd68fe21fa2a6a9
first-name second-name <silvia.wachira@student.moringa.com>
2022-08-21 15:17:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,74 @@
 // Code your solution here
+
+const drivers = [
+
+    {
+        lastName: "Bobby",
+        address: "109 Manhattan",
+
+    },
+    {
+        lastName: "Wachira",
+        address: "93 Brooklyn",
+
+    },
+    {
+        lastName: "Simbine",
+        address: "34 RSA",
+
+    },
+    {
+        lastName: "Manyonga",
+        address: "7 Bahamas",
+
+    }
+]
+// function findMatching(drivers,lastName) {
+    
+//     for (const user of drivers) {
+//         if (lastName === "Bobby") {
+//             console.log(lastName)
+//         }
+//     }
+// }
+// findMatching(drivers)
+
+
+function findMatching(drivers,name) {
+    return drivers.filter(function (driver) {
+        return driver.toLowerCase() === name.toLowerCase()
+    });
+};
+
+
+function fuzzyMatch(drivers,driverName) {
+    return drivers.filter(function(driver) {
+        return driver.slice(0,driverName.length ).toLowerCase() === driverName.toLowerCase()
+        });
+    };
+
+   function matchName(drivers, driverMatch) {
+        return drivers.filter(function(driver) {
+            return driver.name.toLowerCase() === driverMatch.toLowerCase()
+        });
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
